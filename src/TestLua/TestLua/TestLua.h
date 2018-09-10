aa8608bcc47e593b4196bfd0da8fde07e0c1eccb
byGeek <byyang@mail.dlut.edu.cn>
2018-09-10 23:15:56 +0800

    initial commit

---
+++ src/TestLua/TestLua/TestLua.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef TESTLUA_EXPORTS
+
+#define TESTLUA_API __declspec(dllexport)	
+#else
+#define TESTLUA_API __declspec(dllimport)
+
+#endif // TESTLUA_EXPORTS
+
+#include "lua.hpp"
+
+extern "C"
+{
+	TESTLUA_API int myadd(lua_State *L);
+
+	TESTLUA_API const struct luaL_Reg mylib[] = {
+		{"myadd", myadd},
+		{NULL, NULL}
+	};
+
+	TESTLUA_API int luaopen_TestLua(lua_State* L) {
+		luaL_newlib(L, mylib);
+		return 1;
+	};
+}
+
