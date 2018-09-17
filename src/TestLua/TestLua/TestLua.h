60d1e7ca68bb8a863c046a1f674dea9f5f9ae801
byGeek <byyang@mail.dlut.edu.cn>
2018-09-17 14:50:18 +0800

    only dllexport luaopen_Testlua function

--- src/TestLua/TestLua/TestLua.h
+++ src/TestLua/TestLua/TestLua.h
@@ -12,13 +12,14 @@
 
 extern "C"
 {
-	TESTLUA_API int myadd(lua_State *L);
+	/*TESTLUA_API*/ int myadd(lua_State *L);
 
-	TESTLUA_API const struct luaL_Reg mylib[] = {
+	/*TESTLUA_API*/ const struct luaL_Reg mylib[] = {
 		{"myadd", myadd},
 		{NULL, NULL}
 	};
 
+	//must dllexport this function
 	TESTLUA_API int luaopen_TestLua(lua_State* L) {
 		luaL_newlib(L, mylib);
 		return 1;
